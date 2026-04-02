9e0aa6f0629202c6509076ea76698c00a9e7f196
veyal <andrew.salim@binus.ac.id>
2026-04-02 13:47:17 +0700

    feat: export InsightsCalendar from Insights barrel

--- client/src/components/Insights/index.js
+++ client/src/components/Insights/index.js
@@ -1,3 +1,4 @@
 export { default as SpendingTrendChart } from './SpendingTrendChart';
 export { default as CategoryBreakdown } from './CategoryBreakdown';
 export { default as DrillDownModal } from './DrillDownModal';
+export { default as InsightsCalendar } from './InsightsCalendar';
